7bd6d1553272ce1b8d99e17628e51ede4474488b
kenshin5996 <lesdon469@gmail.com>
2026-05-02 16:04:02 +0200

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,8 +0,0 @@
-window.RDM_FIREBASE_CONFIG = {
-  apiKey: "AIzaSyB04km_rnFMLL0oCnxIRT7xVCuJKNMvXw",
-  authDomain: "team-rdm-site.firebaseapp.com",
-  projectId: "team-rdm-site",
-  storageBucket: "team-rdm-site.firebasestorage.app",
-  messagingSenderId: "995665285465",
-  appId: "1:995665285465:web:ca1f40525e0b6768e1bc48"
-};
